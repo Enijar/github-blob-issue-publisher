07c2bf23384194ee115d5eb7b8181406cb157add
James <jamescraig@finervision.com>
2018-09-02 15:25:19 +0100

    Changed repository and added const for pages path

--- src/consts.js
+++ src/consts.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const BLOG_LABEL_NAME = 'blog';
 const USERNAME = 'Enijar';
-const REPOSITORY = 'web-user-activity-recorder';
-const BLOGS_FILE = path.resolve(__dirname, '..', 'storage', 'blogs.json');
+const REPOSITORY = 'github-blob-issue-publisher';
+const BLOGS_FILE = path.resolve(__dirname, '..', 'storage', 'data', 'blogs.json');
+const PAGES_PATH = path.resolve(__dirname, '..', 'storage', 'pages');
 
 module.exports = {
     BLOG_LABEL_NAME,
     USERNAME,
     REPOSITORY,
-    BLOGS_FILE
+    BLOGS_FILE,
+    PAGES_PATH
 };
