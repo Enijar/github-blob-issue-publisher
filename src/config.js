99c4b4f6910aa679a790b1b67fd9ff6e4c30430f
James <jamescraig@finervision.com>
2018-09-02 17:31:53 +0100

    Removed env.* files and placed their contents inside config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const env = require('../env');
 
 module.exports = {
-    blogLabel: env.blogLabel,
-    username: env.username,
-    repository: env.repository,
+    blogLabel: 'blog',
+    username: 'Enijar',
+    repository: 'github-blob-issue-publisher',
     blogsFile: path.resolve(__dirname, '..', 'storage', 'blogs.json'),
     publicPath: path.resolve(__dirname, '..', 'public'),
     viewsPath: path.resolve(__dirname, '..', 'views'),
