5cdca1f0754f7522d9ce55e59b42b69758df8f98
James <jamescraig@finervision.com>
2018-09-02 17:46:20 +0100

    Fix for no blogs.json file

--- src/Functions/FindBlogBySlug.js
+++ src/Functions/FindBlogBySlug.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const config = require('../config');
 
 module.exports = slug => {
+    if (!fs.existsSync(config.blogsFile)) {
+        return null;
+    }
+
     const blogs = JSON.parse(fs.readFileSync(config.blogsFile, 'utf-8') || '[]');
-    return blogs.find(blog => blog.slug === slug);
+    return blogs.find(blog => blog.slug === slug) || null;
 };
