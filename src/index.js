bcecd69ae55801e7b64a414ee4cb96c21a56ed9b
James <jamescraig@finervision.com>
2018-09-02 18:06:56 +0100

    Optional process ENV port for server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const config = require('./config');
 const routes = require('./routes');
+
 const app = express();
+const port = process.env.PORT || config.port;
 
 app.use(express.static(config.publicPath));
 
 routes(app);
 
-app.listen(config.port, () => console.log(`Running app on port ${config.port}`));
+app.listen(port, () => console.log(`Running app on port ${port}`));
