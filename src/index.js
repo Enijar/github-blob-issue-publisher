cd6c30b7ab92b349a54c682eda0aeb065ef20f62
James <jamescraig@finervision.com>
2018-09-02 15:24:57 +0100

    Added GeneratePagesFromBlogs command call to index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const FetchBlogs = require('./Commands/FetchBlogs');
 const CreateOrUpdateBlogs = require('./Commands/CreateOrUpdateBlogs');
+const GeneratePagesFromBlogs = require('./Commands/GeneratePagesFromBlogs');
 
 (async () => {
     try {
         const blogs = await FetchBlogs();
         await CreateOrUpdateBlogs(blogs);
+        await GeneratePagesFromBlogs();
     } catch (err) {
         console.error(err);
     }
